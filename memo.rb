f9d4b373f36b7d9e42603284e5eaa27f8d11c5b8
SeijiNumata <seizi.numata@gmail.com>
2020-12-22 21:49:19 +0900

    レビュー反映

--- memo.rb
+++ memo.rb
@@ -4,19 +4,25 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
 ['/memos', '/'].each do |path|
 get path do
-  @memo_files = Dir.glob('models/*').sort { |a, b| File.stat(a).birthtime <=> File.stat(b).birthtime }
+  # @memo_files = Dir.glob('models/*').sort { |a, b| File.stat(a).birthtime <=> File.stat(b).birthtime }
+  memo_files=Dir.glob('models/*').sort{ |a, b| File.ctime(a) <=> File.ctime(b)}
   @memos = []
-  @id = []
-  @memo_files.each do |memo_file|
+  memo_files.each do |memo_file|
     File.open(memo_file.to_s, 'r') do |j|
       hash = JSON.parse(j.read)
       hash['title'] = '無題のタイトル' if hash['title'] == ''
-      @title_id = []
-      @title_id << hash['title']
-      @title_id << hash['id']
-      @memos << @title_id
+      title_id = []
+      title_id << hash['title']
+      title_id << hash['id']
+      @memos << title_id
     end
   end
   erb :main
@@ -28,9 +34,9 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  @title = params[:title]
-  @content = params[:content]
-  memo = { "id": SecureRandom.uuid, "title": @title, "content": @content }
+  title = h(params[:title])
+  content = h(params[:content])
+  memo = { "id": SecureRandom.uuid, "title": title, "content": content }
   File.open("models/#{memo[:id]}.json", 'w') do |io|
     io.puts(JSON.pretty_generate(memo))
   end
@@ -38,14 +44,14 @@ post '/memos' do
 end
 
 get '/memos/:id' do
-  open("models/#{params[:id]}.json") do |io|
+  open("models/#{h(params[:id])}.json") do |io|
     @memo = JSON.parse(io.read)
   end
   erb :show
 end
 
 get '/memos/:id/edit' do
-  open("models/#{params[:id]}.json") do |io|
+  open("models/#{h(params[:id])}.json") do |io|
     @memo = JSON.parse(io.read)
   end
   erb :edit
@@ -54,10 +60,10 @@ end
 enable :method_override
 
 patch '/memos/:id' do
-  @title = params[:title]
-  @content = params[:content]
+  title = h(params[:title])
+  content = h(params[:content])
   id = params['id']
-  memo = { "id": id, "title": @title, "content": @content }
+  memo = { "id": id, "title": @title, "content": content }
   File.open("models/#{id}.json", 'w') do |io|
     io.puts(JSON.pretty_generate(memo))
   end
